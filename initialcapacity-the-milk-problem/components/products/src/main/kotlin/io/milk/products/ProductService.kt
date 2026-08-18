c9ffaf8ab9b99a324f9d3bd5078b39b3717066ea
justfocus99 <justfocus99@outlook.com>
2026-08-18 16:10:43 -0400

    Fix lost-update race by using atomic decrementBy on purchase

--- initialcapacity-the-milk-problem/components/products/src/main/kotlin/io/milk/products/ProductService.kt
+++ initialcapacity-the-milk-problem/components/products/src/main/kotlin/io/milk/products/ProductService.kt
@@ -18,6 +18,6 @@ class ProductService(private val dataGateway: ProductDataGateway) {
     }
 
     fun decrementBy(purchase: PurchaseInfo) {
-        // TODO - Implement the function.
+        dataGateway.decrementBy(purchase)
     }
 }
